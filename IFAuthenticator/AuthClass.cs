ae393f1ce9dfbb2358bb9dcdada049dbb2bf8e74
Hugh Jones <hugh@37ChapelLane.co.uk>
2024-04-22 12:26:20 +0100

    authtype basic

--- IFAuthenticator/AuthClass.cs
+++ IFAuthenticator/AuthClass.cs
@@ -148,6 +148,7 @@ namespace IFAuthenticator.Controllers
                 LdapDirectoryIdentifier identifier = new(_ldapSettings.Server, _ldapSettings.Port);
                 LdapConnection connection = new LdapConnection(identifier);
                 connection.SessionOptions.SecureSocketLayer = _ldapSettings.UseSSL;
+                connection.AuthType = AuthType.Basic;
                 connection.SessionOptions.ProtocolVersion = 3;
                 NetworkCredential credential = new($"{username}@{_ldapSettings.Domain}", password);
                 connection.Credential = credential;
