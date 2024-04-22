cccf22e4ca81d5687763f8fe0d5282c1e97bb60b
Hugh Jones <hugh@37ChapelLane.co.uk>
2024-04-22 13:54:51 +0100

    btauth pdb permissions ok

--- IFGlobal/PortResolver.cs
+++ IFGlobal/PortResolver.cs
@@ -1,18 +1,11 @@
-﻿namespace IFGlobal
+﻿using Microsoft.Extensions.Logging;
+
+namespace IFGlobal
 {
     public class PortResolver
     {
         static string? _appName;
 
-        private static string AppName 
-        {
-            get 
-            { 
-                if(_appName is null) 
-                    _appName = System.Reflection.Assembly.GetExecutingAssembly().GetName().Name;
-                return _appName!;
-            }
-        }
         public static int GetPort()
         {
             return GetPort(AppName);
@@ -20,14 +13,30 @@
 
         public static int GetPort(string appname)
         {
-
             switch (appname)
             {
+                case "BreakTackleAPI": return 5001;
+                case "JibberJabber": return 5002;
                 case "BTAuthenticator": return 5003;
                 case "IFAuthenticator": return 5004;
-            }
 
-            return 5000;
+                default:
+                    string msg = $"PortResolver Does Not Recognise {appname}";
+
+                    Console.WriteLine(msg);
+                    
+                    throw new ArgumentException(msg);
+            }
+        }
+
+        private static string AppName
+        {
+            get
+            {
+                if (_appName == null)
+                    _appName = System.Reflection.Assembly.GetExecutingAssembly().GetName().Name;
+                return _appName!;
+            }
         }
     }
 }
