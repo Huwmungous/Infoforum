2a83d40d3ec90cda2a0bd15a795b82066546aa95
Hugh Jones <hugh@37ChapelLane.co.uk>
2024-05-01 12:22:58 +0100

    touch

--- IFAuthenticator/LDAPSettings.cs
+++ IFAuthenticator/LDAPSettings.cs
@@ -6,6 +6,6 @@
         public string Server { get; set; } = Environment.GetEnvironmentVariable("LDAP_SERVER") ?? "localhost";
         public int Port { get { return UseSSL ? 636 : 389; } }
         public string Domain { get; set; } = Environment.GetEnvironmentVariable("LDAP_DOMAIN") ?? "longmanrd.infoforum.co.uk";
-        public bool UseSSL { get; set; } = true;
+        public bool UseSSL { get; set; } = false;
     }
 } 
