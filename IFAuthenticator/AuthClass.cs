c895ef03edfda48c90ccc9cd505af5d2800c7d76
Hugh Jones <hugh@37ChapelLane.co.uk>
2024-05-01 12:15:59 +0100

    try the Novell approach

--- IFAuthenticator/AuthClass.cs
+++ IFAuthenticator/AuthClass.cs
@@ -1,9 +1,8 @@
-﻿using System.DirectoryServices.Protocols;
-using System.DirectoryServices.AccountManagement;
-using System.Net;
+﻿using Novell.Directory.Ldap;
 using Microsoft.Extensions.Options;
 using System.Timers;
 using System.Collections.Concurrent;
+using System.Net;
 
 namespace IFAuthenticator.Controllers
 {
@@ -14,30 +13,31 @@ namespace IFAuthenticator.Controllers
         private static readonly ConcurrentDictionary<string, List<string>> _claims = new();
         private static readonly ConcurrentDictionary<string, UserPass> _authenticatedUsers = new();
 
-        private static readonly string serviceuser = Environment.GetEnvironmentVariable("LDAP_SERVICE_USER") ?? "";
+        private static readonly string serviceUser = Environment.GetEnvironmentVariable("LDAP_SERVICE_USER") ?? "";
         private static readonly string servicePass = Environment.GetEnvironmentVariable("LDAP_SERVICE_PASSWORD") ?? "";
 
-        private static LdapConnection? _claimsConnection;
+        private static LdapConnection _claimsConnection;
         private static DateTime _lastClaimsRequest;
 
         public LdapConnection ClaimsConnection
         {
-            get 
+            get
             {
                 _lastClaimsRequest = DateTime.UtcNow;
 
-                _claimsConnection = _claimsConnection ?? GetLdapConnection(serviceuser, servicePass);
+                if (_claimsConnection == null || !_claimsConnection.Connected)
+                {
+                    _claimsConnection = GetLdapConnection(serviceUser, servicePass);
+                }
 
-                return _claimsConnection!;
+                return _claimsConnection;
             }
         }
 
         public AuthClass(ILogger<AuthClass> logger, IOptions<LdapSettings> ldapSettings)
         {
             _logger = logger;
-
             _ldapSettings = ldapSettings.Value;
-
             InitializeCleanupTimer();
         }
 
@@ -45,31 +45,26 @@ namespace IFAuthenticator.Controllers
 
         private void InitializeCleanupTimer()
         {
-            if (_cleanupTimer is null)
+            if (_cleanupTimer == null)
             {
-                _cleanupTimer = new System.Timers.Timer(59999); // Set the interval to about 60000 milliseconds (1 minute)
-                _cleanupTimer.Elapsed += (object? sender, ElapsedEventArgs e) =>
+                _cleanupTimer = new System.Timers.Timer(60000);
+                _cleanupTimer.Elapsed += (sender, e) =>
+                {
+                    if ((_lastClaimsRequest.AddMinutes(5) < DateTime.UtcNow) && _claimsConnection != null)
                     {
-                        // Close Claims ldap connection if it has been unused for 5 minutes, or so
-                        if ((_lastClaimsRequest.AddMinutes(5) < DateTime.UtcNow) && _claimsConnection is not null)
-                        {
-                            _claimsConnection.Dispose();
-                            _claimsConnection = null;
-                            _logger.LogInformation("Inactive Claims Connection Closed.");
-                        }
+                        _claimsConnection.Dispose();
+                        _claimsConnection = null;
+                        _logger.LogInformation("Inactive Claims Connection Closed.");
+                    }
 
-                        // Remove expired authenticated users and their claims
-                        var expiredTokens = _authenticatedUsers.Where(kvp => kvp.Value.Expires <= DateTime.UtcNow)
-                                                               .Select(kvp => kvp.Key)
-                                                               .ToList(); // To avoid collection modification during enumeration
-
-                        foreach (var token in expiredTokens)
-                        {
-                            _authenticatedUsers.TryRemove(token, out var removedUser);
-                            _claims.TryRemove(token, out var removedClaims);
-                            _logger.LogInformation($"Removed Expired User {removedUser!.User} and Claims.");
-                        }
-                    };
+                    var expiredTokens = _authenticatedUsers.Where(kvp => kvp.Value.Expires <= DateTime.UtcNow).ToList();
+                    foreach (var token in expiredTokens)
+                    {
+                        _authenticatedUsers.TryRemove(token.Key, out var removedUser);
+                        _claims.TryRemove(token.Key, out var removedClaims);
+                        _logger.LogInformation($"Removed Expired User {removedUser!.User} and Claims.");
+                    }
+                };
                 _cleanupTimer.AutoReset = true;
                 _cleanupTimer.Enabled = true;
             }
@@ -82,133 +77,86 @@ namespace IFAuthenticator.Controllers
             {
                 try
                 {
-                    using (var context = new PrincipalContext(ContextType.Domain, _ldapSettings.Domain))
-                    {
-                        if (context.ValidateCredentials(username, password))
-                        {
-                            string token = Guid.NewGuid().ToString().Replace("-", "");
-                            _authenticatedUsers[token] = new() { User = username, Pass = password };
-                            _claims[token] = [];
-                            result = (true, token);
-                        }
-                    }
-                }
-                catch (Exception ex)
-                {
-                    // Handle or log the error as needed
-                    _logger.LogError($"An error occurred: {ex.Message}");
-                }
-            });
-
-            return result;
-        }
-
-        /*
-        public async Task<(bool isAuthenticated, string token)> AuthenticateUserAsync(string username, string password)
-        {
-            var result = (false, "");
-
-            await Task.Run(() =>
-            {
-                try
-                {
                     using var conn = GetLdapConnection(username, password);
-                    if (conn is not null)
+                    if (conn != null && conn.Connected)
                     {
                         string token = Guid.NewGuid().ToString().Replace("-", "");
-                        _authenticatedUsers[token] = new() { User = username, Pass = password };
-                        _claims[token] = [];
+                        _authenticatedUsers[token] = new UserPass { User = username, Pass = password };
+                        _claims[token] = new List<string>();
                         result = (true, token);
                     }
                 }
-                catch (LdapException ex)
-                {
-                    _logger.LogError($"Login Failed: {ex.Message}");
-                }
                 catch (Exception ex)
                 {
-                    _logger.LogError($"An error occurred: {ex.Message}");
+                    _logger.LogError($"Authentication error: {ex.Message}");
                 }
-            }).ConfigureAwait(false);
-
+            });
             return result;
         }
-        */
 
         public async Task<bool> AuthoriseAsync(string token, string claim)
         {
+            return await Task.Run(() =>
+            {
                 try
                 {
-                    return _authenticatedUsers.ContainsKey(token) && 
-                          (CacheHasClaim(token, claim) || await LdapHasClaim(token, claim));
+                    return _authenticatedUsers.ContainsKey(token) && (CacheHasClaim(token, claim) || LdapHasClaim(token, claim).Result);
                 }
-                catch(Exception ex)
+                catch (Exception ex)
                 {
-                    _logger.LogError($"An error occurred: {ex.Message}");
-
+                    _logger.LogError($"Authorization error: {ex.Message}");
                     return false;
                 }
- 
+            });
         }
 
         private async Task<bool> LdapHasClaim(string token, string claim)
         {
             var result = false;
-
             await Task.Run(() =>
             {
                 try
                 {
                     var userPass = _authenticatedUsers[token];
-
-                    if (userPass is null)
+                    if (userPass == null)
+                    {
                         throw new Exception($"Token not found {token}");
+                    }
 
-                    if (serviceuser is null || servicePass is null)
-                        throw new ArgumentException("Failed to Identify the ldap Service Account or Password");
-                    
-                    SearchRequest searchRequest = new("DC=longmanrd,DC=infoforum,DC=co,DC=uk", $"(&(objectClass=group)(cn={claim}))", SearchScope.Subtree, "member");
+                    if (string.IsNullOrEmpty(serviceUser) || string.IsNullOrEmpty(servicePass))
+                    {
+                        throw new ArgumentException("Service account credentials are not set.");
+                    }
 
-                    var response = (SearchResponse)ClaimsConnection.SendRequest(searchRequest);
-
-                    result = CheckMemberships(token, claim, result, userPass, response);
-                }
-                catch (LdapException ex)
-                {
-                    _logger.LogError($"LDAP error occurred: {ex.Message}");
+                    var searchFilter = $"(&(objectClass=group)(cn={claim}))";
+                    var searchBase = "DC=your,DC=domain,DC=com";
+                    var search = _claimsConnection.Search(searchBase, LdapConnection.ScopeSub, searchFilter, new string[] { "member" }, false);
+                    while (search.HasMore())
+                    {
+                        var entry = search.Next();
+                        var members = entry.GetAttributeSet()["member"];
+                        if (members != null)
+                        {
+                            foreach (var member in members.StringValueArray)
+                            {
+                                if (member.Contains(userPass.User, StringComparison.OrdinalIgnoreCase))
+                                {
+                                    if (!_claims[token].Contains(claim))
+                                    {
+                                        _claims[token].Add(claim);
+                                    }
+                                    result = true;
+                                    break;
+                                }
+                            }
+                        }
+                    }
                 }
                 catch (Exception ex)
                 {
-                    _logger.LogError($"An error occurred: {ex.Message}");
+                    _logger.LogError($"LDAP query error: {ex.Message}");
                 }
             });
-
-            // If no entries are found or the user is not a member of the group, return false.
-            return result;
-        }
-
-        private static bool CheckMemberships(string token, string claim, bool result, UserPass? userPass, SearchResponse response)
-        {
-            // Check each group found (though typically should be just one) for user membership
-            foreach (SearchResultEntry entry in response.Entries)
-            {
-                // Check if the user is listed as a member of the group
-                foreach (string member in entry.Attributes["member"].GetValues(typeof(string)))
-                {
-                    if (member.Contains(userPass!.User, StringComparison.OrdinalIgnoreCase))
-                    {
-                        // Optionally, cache the claim
-                        if (!_claims[token].Contains(claim))
-                        {
-                            _claims[token].Add(claim);
-                        }
-                        result = true; // User is a member of the group
-
-                        break;
-                    }
-                }
-            }
-
             return result;
         }
 
@@ -217,31 +165,34 @@ namespace IFAuthenticator.Controllers
             return _claims[token].Contains(claim);
         }
 
-        private LdapConnection? GetLdapConnection(string username, string password)
+        private LdapConnection GetLdapConnection(string username, string password)
         {
-            _logger.LogInformation($"{_ldapSettings.Server}:{_ldapSettings.Port} user {username}@{_ldapSettings.Domain} attempting to connect");
             try
             {
-                var connection = new LdapConnection(
-                    new LdapDirectoryIdentifier(_ldapSettings.Server, _ldapSettings.Port),
-                    new NetworkCredential($"{username}@{_ldapSettings.Domain}", password),
-                    AuthType.Kerberos
-                );
+                var connection = new LdapConnection();
+                // Specify SSL and the appropriate port if necessary
+                connection.SecureSocketLayer = _ldapSettings.UseSSL;
 
-                connection.SessionOptions.ReferralChasing = ReferralChasingOptions.None;
-                connection.SessionOptions.SecureSocketLayer = _ldapSettings.UseSSL;
-                connection.Bind();
+                if (_ldapSettings.UseSSL)
+                {
+                    // Default port for LDAPS is usually 636, ensure it's set correctly in _ldapSettings
+                    connection.Connect(_ldapSettings.Server, _ldapSettings.Port);
+                    connection.Bind(LdapConnection.LdapV3, $"{username}@{_ldapSettings.Domain}", password);
+                }
+                else
+                {
+                    connection.Connect(_ldapSettings.Server, _ldapSettings.Port);
+                    connection.Bind($"{username}@{_ldapSettings.Domain}", password);
+                }
+
                 return connection;
             }
             catch (Exception ex)
             {
-                _logger.LogError($"Connection Failed for user {username}@{_ldapSettings.Domain} : {ex}");
-
+                _logger.LogError($"LDAP connection error: {ex.Message}");
                 return null;
             }
         }
 
-        // Implement cleanup routines for expired tokens and claims
     }
-
 }
