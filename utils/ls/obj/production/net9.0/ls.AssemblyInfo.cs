fe61e12f8d5a474dc200515e140e04bc2714863d
Huwmungous <67276132+Huwmungous@users.noreply.github.com>
2025-02-05 13:15:08 +0000

    first-cut *nix translation

--- utils/ls/obj/production/net9.0/ls.AssemblyInfo.cs
+++ utils/ls/obj/production/net9.0/ls.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ls")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("production")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89966563c2581eb4e889bc6f542270c4cf0673b8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f32153d118b265401d7285be68a469a63dad2af")]
 [assembly: System.Reflection.AssemblyProductAttribute("ls")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ls")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
