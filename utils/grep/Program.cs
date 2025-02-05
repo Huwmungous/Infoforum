a61568680582cc8aabda4b8462a2f39e412b3109
Huwmungous <67276132+Huwmungous@users.noreply.github.com>
2025-02-05 11:24:40 +0000

    Parse Switch & Parse Command Line

--- utils/grep/Program.cs
+++ utils/grep/Program.cs
@@ -1,52 +1,41 @@
 ﻿using System;
 using System.IO;
 
-class Program
+class Grep
 {
+    static bool useStdin = false;
+    static string searchPattern = "";
+    static string inputFilePath = ".";
+    static string outputFilePath = "";
+    static bool writeToConsole = true;
+    static bool recursive = false;
+
     static void Main(string[] args)
     {
-        bool useStdin = false;
-        string searchPattern = "";
-        string inputFilePath = "";
-        string outputFilePath = "";
-        bool writeToConsole = true;
-        bool recursive = false;
-
-        // Parse command line arguments to determine options and inputs
-        for (int i = 0; i < args.Length; i++)
+        if (args.Length == 0)
         {
-            if (args[i] == "-r")
-            {
-                recursive = true;
-            }
-            else if (useStdin == false)
-            {
-                if (i + 1 < args.Length && args[i + 1] != "-r" && !args[i + 1].StartsWith("-"))
-                {
-                    inputFilePath = args[i];
-                    outputFilePath = args[++i];
-                }
-                else
-                {
-                    searchPattern = args[i];
-                }
-            }
+            Console.WriteLine("Usage: grep <search_pattern> [-r] [<file_path>] [<output_file_path>]");
+            return;
         }
+        else
+            Grep.searchPattern = args[0];
 
-        if (args.Length == 0 || (useStdin == false && string.IsNullOrEmpty(inputFilePath)))
+        parseCommandLine(args);
+
+        if (Grep.useStdin == false && string.IsNullOrEmpty(Grep.inputFilePath))
         {
             Console.WriteLine("Usage: grep <search_pattern> [-r] [<file_path>] [<output_file_path>]");
             return;
         }
 
-        if (useStdin)
+        if (Grep.useStdin)
         {
             // Read from stdin
-            while ((inputFilePath = Console.ReadLine()) != null)
+            while ((Grep.inputFilePath? = Console.ReadLine()) != null)
             {
-                if (recursive || inputFilePath.Contains(searchPattern))
+                if (Grep.recursive || Grep.inputFilePath.Contains(searchPattern))
                 {
-                    WriteLineToDestination(inputFilePath, outputFilePath, writeToConsole);
+                    WriteLineToDestination(Grep.inputFilePath, Grep.outputFilePath, Grep.writeToConsole);
                 }
             }
         }
@@ -60,6 +49,36 @@ class Program
             catch (Exception ex)
             {
                 Console.WriteLine($"An error occurred: {ex.Message}");
+            }
+        }
+    }
+
+    static void parseSwitch(string sw)
+    {
+        if(sw == "-r")
+          recursive = true;
+    }
+
+    static void parseCommandLine(string[] args)
+    {
+        for (int i = 1; i < args.Length; i++)
+        {
+            Console.WriteLine(args[i]);
+            
+            if (args[i].StartsWith("-")) 
+              parseSwitch(args[i]);
+
+            else if (!useStdin)
+            {
+                if (i + 1 < args.Length && args[i + 1] != "-r" && !args[i + 1].StartsWith("-"))
+                {
+                    inputFilePath = args[i];
+                    outputFilePath = args[++i];
+                }
+                else
+                {
+                    searchPattern = args[i];
+                }
             }
         }
     }
