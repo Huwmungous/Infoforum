6504b175f4bf5bc3eaddc73a01b507df2a08284f
Huwmungous <67276132+Huwmungous@users.noreply.github.com>
2025-02-21 09:57:21 +0000

    creating auth-lib library

---
+++ IFLibraries/projects/ifauth-lib/src/lib/ifauth-lib.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-IFAuthLib',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      ifauth-lib works!
+    </p>
+  `,
+  styles: ``
+})
+export class IFAuthLibComponent {
+
+}
---
+++ IFLibraries/projects/ifauth-lib/src/public-api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of ifauth-lib
+ */
+
+export * from './lib/ifauth-lib.service';
+export * from './lib/ifauth-lib.component';
