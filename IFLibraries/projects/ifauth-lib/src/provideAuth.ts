f278c808f6036554f654a136f1fd50c7a75af9a9
Huwmungous <67276132+Huwmungous@users.noreply.github.com>
2025-02-21 17:34:50 +0000

    touch

--- IFLibraries/projects/ifauth-lib/src/provideAuth.ts
+++ IFLibraries/projects/ifauth-lib/src/provideAuth.ts
@@ -6,10 +6,6 @@ export function provideAuth(realm: string = '', client: string = '') {
 
   const issuer =  'https://longmanrd.net/auth/realms/' + (realm ? realm : 'LongmanRd'); //default realm
   const clnt = client ? client : '53FF08FC-C03E-4F1D-A7E9-41F2CB3EE3C7'; // default clientId
-
-  console.log('Issuer: ',issuer);
-  console.log('Client : ', clnt);  
-
   const cfg : OpenIdConfiguration = {
     authority: issuer,
     redirectUrl: window.location.origin + '/auth-callback',
