41365e5fc59dbedf216345c5236ad32231ef4cdc
Huwmungous <67276132+Huwmungous@users.noreply.github.com>
2025-02-22 12:11:55 +0000

    touch

--- testapp/src/app/app.component.ts
+++ testapp/src/app/app.component.ts
@@ -3,11 +3,10 @@ import { RouterOutlet } from '@angular/router';
 import { LogoutService } from 'ifauth-lib';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    imports: [RouterOutlet],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'testapp';
