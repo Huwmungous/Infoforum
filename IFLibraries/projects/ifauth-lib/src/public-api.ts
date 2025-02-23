d6a78b3845ed3d9f271f78910d1e79bf34c321b0
Huwmungous <67276132+Huwmungous@users.noreply.github.com>
2025-02-23 10:43:10 +0000

    touch

--- IFLibraries/projects/ifauth-lib/src/public-api.ts
+++
@@ -1,8 +0,0 @@
-/*
- * Public API Surface of ifauth-lib
- */
-export * from './ifauth.module';
-export * from './auth.guard';
-export * from './components/auth-callback-component/auth-callback.component';
-export * from './logout-service';
-export * from './provideAuth';
