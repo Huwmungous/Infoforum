3496a2704a520a52fb03d3c8bcbafbba62bcfd2a
Huwmungous <Hugh@37ChapelLane.co.uk>
2025-02-28 19:15:51 +0000

    touch

--- Angular/projects/deepseek-gui/deploy.sh
+++ Angular/projects/deepseek-gui/deploy.sh
@@ -4,12 +4,14 @@ cd ../..
 
 sudo ng build IFSharedLibrary --configuration production
 
-sudo ng build --configuration production
+cd ./projects/deepseek-gui
+
+ng build deepseek-gui --base-href /intelligence/ --configuration production
 
 sudo mkdir -p /var/www/Intelligence/
 sudo rm -rf /var/www/Intelligence/*
 
-sudo cp -r /home/hugh/repos/Infoforum/Angular/projects/deepseek-gui/dist/deepseek-gui/* /var/www/Intelligence/
+sudo cp -r  /home/hugh/repos/Infoforum/Angular/projects/deepseek-gui/dist/deepseek-gui/* /var/www/Intelligence/
 
 sudo chown -R nginx:nginx /var/www/Intelligence
 sudo chmod -R 755 /var/www/Intelligence
