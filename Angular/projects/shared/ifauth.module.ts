350fc8503f24828aa8cb130976d5f262df15e4cb
Huwmungous <Hugh@37ChapelLane.co.uk>
2025-03-02 10:50:25 +0000

    provide interceptors

--- Angular/projects/shared/ifauth.module.ts
+++ Angular/projects/shared/ifauth.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AuthConfigService } from './ifauth/auth-config.service';
 import { AuthGuard } from './ifauth/auth.guard';
 import { IFTokenInterceptor } from './ifauth/token-interceptor';
 import { AuthCallbackComponent } from './ifauth/auth-callback.component';
+import { ClientService } from './ifauth/client.service';   
 
 @NgModule({
   imports: [
@@ -13,7 +15,12 @@ import { AuthCallbackComponent } from './ifauth/auth-callback.component';
   providers: [
     AuthConfigService,
     AuthGuard,
-    IFTokenInterceptor
+    ClientService,  
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: IFTokenInterceptor,
+      multi: true   
+    }
   ],
   exports: [
     AuthCallbackComponent
