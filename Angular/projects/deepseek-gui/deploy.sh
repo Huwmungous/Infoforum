1d7e4a2d698dc427a55c8d605492a472cf7b9a8c
Huwmungous <Hugh@37ChapelLane.co.uk>
2025-03-03 12:56:24 +0000

    touch

--- Angular/projects/deepseek-gui/deploy.sh
+++ Angular/projects/deepseek-gui/deploy.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-# Navigate to the workspace root where IFAuthModule is located
-cd ../..
-
-# Build the IFAuthModule
-sudo ng build IFAuthModule --configuration production
-
-# Change directory back to deepseek-gui
-cd ./projects/deepseek-gui
-
-# Build the deepseek-gui application, making sure it includes IFAuthModule
 ng build deepseek-gui --base-href /intelligence/ --configuration production
 
 sudo mkdir -p /var/www/Intelligence/
