be544c8a67b4b0f013a888db1bb3450d81475b40
Huwmungous <67276132+Huwmungous@users.noreply.github.com>
2025-03-04 15:02:24 +0000

    touch

--- Angular/projects/deepseek-gui/deploy.sh
+++ Angular/projects/deepseek-gui/deploy.sh
@@ -3,6 +3,8 @@
 # Build the library first
 cd ../..
 
+sudo rm -rf ./dist/*
+
 ng build ifauth-lib --configuration production
 
 # Now build the application
@@ -11,7 +13,7 @@ ng build deepseek-gui --configuration production
 sudo mkdir -p /var/www/Intelligence/
 sudo rm -rf /var/www/Intelligence/*
 
-sudo cp -r /home/hugh/repos/Infoforum/Angular/projects/deepseek-gui/dist/deepseek-gui/* /var/www/Intelligence/
+sudo cp -r ./dist/* /var/www/Intelligence/
 
 sudo chown -R nginx:nginx /var/www/Intelligence
 sudo chmod -R 755 /var/www/Intelligence
