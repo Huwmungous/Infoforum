c1d89ceaf70c1a12febfb5187eeaf56c6e1d27bd
Huwmungous <67276132+Huwmungous@users.noreply.github.com>
2025-03-05 10:54:59 +0000

    default config -> development

--- Angular/projects/deepseek-gui/src/environments/environment.ts
+++ Angular/projects/deepseek-gui/src/environments/environment.ts
@@ -1,11 +1,5 @@
-// export const environment = {
-//     production: false,
-//     apiUrl: 'http://10.9.8.3:5008/IFOllama', 
-//     consoleLog: true
-//   };
-
 export const environment = {
-  production: true,
-  apiUrl: 'https://longmanrd.net/intelligence/api', 
-  consoleLog: true
-};
+    production: false,
+    apiUrl: 'http://10.9.8.3:5008/IFOllama', 
+    consoleLog: true
+  };
