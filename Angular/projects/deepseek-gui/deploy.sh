f98c4ac4674289a100102f40559ad0e4e4b275dc
Huwmungous <67276132+Huwmungous@users.noreply.github.com>
2025-03-05 16:59:55 +0000

    touch

--- Angular/projects/deepseek-gui/deploy.sh
+++ Angular/projects/deepseek-gui/deploy.sh
@@ -9,7 +9,7 @@ npm run build-and-deploy || { echo "Error building and deploying library!"; exit
 sudo mkdir -p /var/www/Intelligence/
 sudo rm -rf /var/www/Intelligence/*
 
-sudo cp -r ../../dist/deepseek-gui/* /var/www/Intelligence/ 
+sudo cp -r ../../dist/* /var/www/Intelligence/ 
 
 sudo chown -R nginx:nginx /var/www/Intelligence
 sudo chmod -R 755 /var/www/Intelligence
