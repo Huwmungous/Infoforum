efc868e0a71ae4b5f980bb9bfff9dcb027395415
Huwmungous <67276132+Huwmungous@users.noreply.github.com>
2025-03-10 14:55:15 +0000

    IF_Logo

--- Angular/projects/deepseek-gui/src/app/ollama.service.ts
+++ Angular/projects/deepseek-gui/src/app/ollama.service.ts
@@ -14,8 +14,6 @@ export class OllamaService {
   sendPrompt(conversationId: string, prompt: string, dest: string = 'code'): Observable<any> {
     const url = `${this.apiUrl}?conversationId=${conversationId}&dest=${dest}`;
     const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
-
-    console.log(`sendPrompt() - url: ${url}, prompt: ${prompt}`);
     return this.http.post(url, JSON.stringify(prompt), { headers, responseType: 'text' });
   }
 }
