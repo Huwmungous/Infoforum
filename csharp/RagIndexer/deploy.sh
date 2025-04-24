ecb31dd4c4f0577b8d880333150286854b6af3c6
Huwmungous <67276132+Huwmungous@users.noreply.github.com>
2025-04-24 10:40:43 +0100

    run the RAGIndexer on deployment

--- csharp/RagIndexer/deploy.sh
+++ csharp/RagIndexer/deploy.sh
@@ -7,3 +7,5 @@ sudo rm -rf /var/www/RagIndexer/*
 dotnet publish --configuration Release --runtime linux-x64 --self-contained true
 
 sudo cp /mnt/ai-data/repos/Infoforum/csharp/RagIndexer/bin/Release/net9.0/linux-x64/* /var/www/RagIndexer -r 
+
+sudo /var/www/RagIndexer/RagIndexer
